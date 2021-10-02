25e1313bc5c53e96af8a27fbe576ad1b910983e4
Hannah <hannahwoodward@hotmail.com>
2021-10-02 19:02:08 +0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: 'Castle Street'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+  }
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Bond St');
+  
+  
+  console.log(updatedEmployee);
+  console.log(employee);
