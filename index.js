eb4200155cce12025a088da2713a7bc9b385d8a5
Hannah <hannahwoodward@hotmail.com>
2021-10-02 19:23:56 +0800

    chechecked answer guide, not able to test

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-// Write your solution in this file!
-const employee = {
-    name: 'Sam',
-    streetAddress: 'Castle Street'
-  };
-  
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, { [key]: value });
-  }
-  
-  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Bond St');
-  
-  
-  console.log(updatedEmployee);
-  console.log(employee);
+let employee = {
+  name: "Hannah",
+  streetAddress: 'Castle Street'
+};
+// console.log(employee);
+
+// question 1
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ return {...employee, [key]: value};
+ return employee;
+}
+//console.log(updateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Bond Street'));
+
+// quesiton 2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+};
+//console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Sam'));
+
+//question 3
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+//console.log(deleteFromEmployeeByKey(employee, 'name'))
+//console.log(employee);
+
+// question 4
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+//console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'));
